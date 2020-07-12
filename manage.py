20ef750608f4f65fe6a19e9b7145e2587308e5a7
yangxu <yngxu@163.com>
2020-07-12 21:04:44 +0800

    默认的

--- manage.py
+++ manage.py
@@ -8,6 +8,7 @@ def main():
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'demo1.settings')
     try:
         from django.core.management import execute_from_command_line
+        print("test")
     except ImportError as exc:
         raise ImportError(
             "Couldn't import Django. Are you sure it's installed and "
